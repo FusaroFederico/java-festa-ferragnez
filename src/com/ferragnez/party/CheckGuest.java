12630cc365bc090250802a74e3690495b29c87d5
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-07 11:49:40 +0200

    add new scanner instance to CheckGuest.java

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,11 +1,14 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
 		// guests array
 		String[] guestsList = { "Dua Lipa", "Paris Hilton" , "Manuel Agnelli" , "J-Ax" , "Francesco Totti" , "Ilary Blasi" , "Bebe Vio" , "Luis" , "Pardis Zarei" , "Martina Maccherone" , "Rachel Zeilic" };
-		
+		// new scanner instance 
+		Scanner scanner = new Scanner(System.in);
 	}
 
 }
