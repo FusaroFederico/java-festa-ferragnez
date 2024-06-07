8e3ad7d72ca428566b82ae501556acce2293fcf1
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-07 11:47:01 +0200

    add guestList array to CheckGuest.java

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -3,8 +3,9 @@ package com.ferragnez.party;
 public class CheckGuest {
 
 	public static void main(String[] args) {
+		// guests array
+		String[] guestsList = { "Dua Lipa", "Paris Hilton" , "Manuel Agnelli" , "J-Ax" , "Francesco Totti" , "Ilary Blasi" , "Bebe Vio" , "Luis" , "Pardis Zarei" , "Martina Maccherone" , "Rachel Zeilic" };
 		
-
 	}
 
 }
